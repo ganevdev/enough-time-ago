d63cc0c42e555ba4dab6a4e3fecaa686e41bd1b3
Ivan Ganev <iganevru@gmail.com>
2019-01-30 17:34:31 +0300

    to longEnough

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-# Is Too Old
+# Long Enough
 
 This is the node library for working with file date.
 
 ```{}
-npm i is-too-old
+npm i long-enough
 ```
 
 Examples with modified (created, changed and accessed - same):
 
 ```{js}
-const isTooOld = require('is-too-old')
+const longEnough = require('long-enough')
 
-isTooOld.modified('./newFile.html')
+longEnough.modified('./newFile.html')
 // return false, if file newFile.html modified <= 86400000 ms
 // by default the millisecond values are 86400000 (one day)
 
-isTooOld.modified('./newFile.html', 10000)
+longEnough.modified('./newFile.html', 10000)
 // return false, if file newFile.html modified  <= 10000 ms
 
-isTooOld.modified('./oldFile.html', 10000)
+longEnough.modified('./oldFile.html', 10000)
 // return true, if file oldFile.html modified > 10000 ms
 
-isTooOld.modified('./nonFile.html', 10000)
+longEnough.modified('./nonFile.html', 10000)
 // return undefined, if such file does not exist
 ```
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,9 +1,28 @@
-import isTooOld from '../index';
+import fs from 'fs';
 
+import longEnough from '../index';
+
+//
 test('Nonexistent file - return undefined', () => {
-  expect(isTooOld.modified('./nonFile.html', 10000)).toBe(undefined);
+  expect(longEnough.modified('./nonFile.html', 10000)).toBe(undefined);
+});
+test('Nonexistent file - return undefined', () => {
+  expect(longEnough.created('./nonFile.html', 10000)).toBe(undefined);
+});
+test('Nonexistent file - return undefined', () => {
+  expect(longEnough.changed('./nonFile.html', 10000)).toBe(undefined);
+});
+test('Nonexistent file - return undefined', () => {
+  expect(longEnough.accessed('./nonFile.html', 10000)).toBe(undefined);
 });
 
-test('Nonexistent file - return undefined', () => {
-  expect(isTooOld.created('./nonFile.html', 10000)).toBe(undefined);
+//
+test('new file - return false', () => {
+  fs.openSync('./newfile.html', 'a');
+  expect(longEnough.created('./newfile.html', 10000)).toBe(false);
+});
+test('old file - return true', () => {
+  fs.openSync('./oldfile.html', 'a');
+
+  expect(longEnough.created('./oldfile.html', 1)).toBe(true);
 });
